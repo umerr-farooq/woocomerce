5697599aded9c21cacbcfe694ada7a56da708642
Umer Farooq <72683902+umerr-farooq@users.noreply.github.com>
2023-06-22 11:39:18 +0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/*** Remove product data tabs ***/
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+
+function woo_remove_product_tabs( $tabs ) {
+    unset( $tabs['description'] );      	// Remove the description tab
+    unset( $tabs['reviews'] ); 			// Remove the reviews tab
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+    return $tabs;
+}
+
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+
+// Add product description under "Add to Cart" button
+function woocommerce_product_description() {
+    global $product;
+
+    if ( $product->get_description() ) {
+        echo '<div class="woocommerce-product-details__short-description woocommerce-product-description">';
+        echo '<h2>Product Description</h2>';
+        echo '<p>' . $product->get_description() . '</p>';
+        echo '</div>';
+    }
+}
+add_action( 'woocommerce_after_add_to_cart_button', 'woocommerce_product_description', 10 );
+
+?>
